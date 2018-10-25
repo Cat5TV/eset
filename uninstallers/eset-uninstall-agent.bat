143018cb13964264d757abdbc47c32e9133c198d
Robbie Ferguson <contact@category5.tv>
2018-10-25 10:38:09 -0400

    Add agent unattended uninstaller

---
+++ uninstallers/eset-uninstall-agent.bat
@@ -0,0 +1,16 @@
+@echo off
+REM This program has to be run as administrator
+REM Removes ESET Remote Administrator Agent
+
+echo "Uninstalling ESET Management Agent..."
+REM Version 6 and under
+wmic product where name="ESET Remote Administrator Agent" call uninstall
+REM Version 7 and up
+wmic product where name="ESET Management Agent" call uninstall
+
+echo "Don't worry if you get errors about missing registry keys."
+echo "Just removing some that may be left behind. If they don't exist, no worries."
+reg delete "HKEY_CLASSES_ROOT\Installer\Products\07F21F149AF55F34494F355BE44BEE4C" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Products\07F21F149AF55F34494F355BE44BEE4C" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Products\07F21F149AF55F34494F355BE44BEE4C" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{41F12F70-5FA9-43F5-94F4-53B54EB4EEC4}" /f
